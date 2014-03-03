ed25656de7b49745bd6425e81b2b7abd8547263b
dnatsvlishvili <d.natsvlishvili@gmail.com>
2014-03-04 01:25:13 +0400

    rowcol to

--- text2table.js
+++ text2table.js
@@ -1,6 +1,10 @@
 var text2table = (function(){
   'use strict';
   
+  var regex, input;
+  var rows;
+  var columns = [], isNumeric = [];
+  
   var data = {
     expression: '\t',
     modifier: 'g'
@@ -18,9 +22,56 @@ var text2table = (function(){
     return target;
   };
   
+  // Global
+  
+  var initialize() {
+    regex = new RegExp(data.expression, data.modifier);
+  };
+  
+  var function getRows() {
+    rows = input.split(/[\r\n]+/);
+    return rows;
+  };
+  
+  var function getCol(n) {
+    return rows[n].split(regex);
+  }
+  
+  var function getCols() {
+    for (var i = 0; i < rows.length; i += 1) {
+      var col = getCol(i);
+      for (var s = 0; s < col.length; s += 1) {
+        var data = col[s];
+        var new = columns[s] == undefined;
+        if (new) {
+          isNumeric[s] = true;
+        }
+        if (new || columns[s] < data.length) {
+          columns[s] = data.length;
+        }
+      }
+    }
+    return columns;
+  }
+ 
+  // toTable
+  
+  
+  // toText
+  
   return {
     data: function(opts) {
       extend(data, opts);
+    },
+    toTable: function(in) {
+      input = in;
+      initialize();
+      
+      if (getRows()[rows.length - 1] === '') { rows.pop(); }
+      getCols();
+    },
+    toText: function() {
+      
     }
   };
 })();
